06b9da444bde75e4414387e75067d418457974a9
Your Name <arfusop@gmail.com>
2019-05-26 16:00:16 -0400

    Got api call for local forecast to work.

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 // import axios from "axios";
 
 export default class App extends Component {
+	state = {
+		data: {}
+	};
 	componentDidMount = async () => {
 		let response;
 		try {
 			// https://crossorigin.me/https://google.com
-
-			response = await fetch(
-				`https://crossorigin.me/${process.env.REACT_APP_API_URL}/forecast/${
-					process.env.REACT_APP_SECRET
-				}/40.7128,74.0060`,
-				{ mode: "cors" }
-			);
+			const proxy = "https://cors-anywhere.herokuapp.com/";
+			const api = `${process.env.REACT_APP_API_URL}forecast/${
+				process.env.REACT_APP_SECRET
+			}/40.7128,74.0060`;
+			const url = proxy + api;
+			const parse = await fetch(url);
+      response = await parse.text();
+      response = JSON.parse(response);
 			console.log(response);
-			console.log("wuuuut");
+			this.setState({
+				data: response
+			});
 		} catch (error) {
 			console.log(error);
 		}
