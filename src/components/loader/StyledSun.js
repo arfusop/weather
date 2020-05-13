b24a823280e6d0e4f0afe8fc285eeb2925f88a91
Phil Arfuso <Arfusop@gmail.com>
2020-05-13 11:09:33 -0400

    Updated the loader style

--- src/components/loader/StyledSun.js
+++ src/components/loader/StyledSun.js
@@ -12,7 +12,7 @@ const StyledSun = styled.section`
     padding-bottom: 25%;
     background-color: #12a4dc;
     border-radius: 2%;
-    border: 1px solid black;
+    box-shadow: 0px 0px 9px 2px rgba(0, 0, 0, 0.75);
 
     .preloader-top {
         position: absolute;
