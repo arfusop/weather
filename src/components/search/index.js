9f59ca41efabdc4589654dee2c6e37661d849eb9
Phil Arfuso <Arfusop@gmail.com>
2020-05-14 13:01:15 -0400

    Added react-icons. Added a transition to the search

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 import { StyledSearch } from './style'
 
-const Search = () => {
+const Search = ({ ...rest }) => {
     return (
-        <StyledSearch>
-            <input type="text" id="locationInput" />
+        <StyledSearch transitionLength="0.6s">
+            <input id="react-google-places-autocomplete-input" {...rest} />
+            <label for="react-google-places-autocomplete-input">
+                <FaSearch className="searchIcon" />
+            </label>
+            <input type="submit" id="search-submit" />
         </StyledSearch>
     )
 }
--- src/components/search/style.js
+++ src/components/search/style.js
@@ -1,5 +1,64 @@
 import styled from '@emotion/styled'
 
-export const StyledSearch = styled.section`
-    
+export const StyledSearch = styled.form`
+    body {
+        margin: 5%;
+    }
+
+    #react-google-places-autocomplete-input {
+        transition: width ${props => props.transitionLength},
+            border-radius ${props => props.transitionLength},
+            background ${props => props.transitionLength},
+            box-shadow ${props => props.transitionLength};
+        width: 40px;
+        height: 40px;
+        border-radius: 20px;
+        border: none;
+        cursor: pointer;
+        background: rgb(235, 235, 235);
+        & + label .searchIcon {
+            color: black;
+        }
+        &:hover {
+            color: white;
+            background: rgb(200, 200, 200);
+            box-shadow: 0 0 0 5px rgb(61, 71, 82);
+            & + label .searchIcon {
+                color: white;
+            }
+        }
+        &:focus {
+            transition: width ${props => props.transitionLength}
+                    cubic-bezier(0, 1.22, 0.66, 1.39),
+                border-radius ${props => props.transitionLength},
+                background ${props => props.transitionLength};
+            border: none;
+            outline: none;
+            box-shadow: none;
+            padding-left: 15px;
+            cursor: text;
+            width: 300px;
+            border-radius: auto;
+            background: rgb(235, 235, 235);
+            color: black;
+            & + label .searchIcon {
+                color: black;
+            }
+        }
+        &:not(:focus) {
+            text-indent: -5000px;
+        } // for more-graceful falling back (:not browsers likely support indent)
+    }
+
+    #search-submit {
+        position: relative;
+        left: -5000px;
+    }
+
+    .searchIcon {
+        position: relative;
+        left: -30px;
+        color: white;
+        cursor: pointer;
+    }
 `
