3186d3f5bc3b9ca13c19348a425865f55c5597d7
Phil Arfuso <Phil@Cathys-MacBook-Pro.local>
2020-05-13 22:06:23 -0400

    Added google places package

--- src/components/autocomplete/index.js
+++ src/components/autocomplete/index.js
@@ -1,47 +1,9 @@
 import React from 'react'
-import Script from 'react-load-script'
-import PropTypes from 'prop-types'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+import 'react-google-places-autocomplete/dist/index.min.css'
 
-let autocomplete = null
-
-const AutoComplete = ({ callback }) => {
-    const handleScriptLoad = () => {
-        console.log('loaded script')
-        const options = { types: ['geocode'] }
-
-        autocomplete = new window.google.maps.places.Autocomplete(
-            document.getElementById('locationInput'),
-            options
-        )
-        // Fire Event when a suggested name is selected
-        autocomplete.addListener('place_changed', () => {
-            // Extract City From Address Object
-            let addressObject = autocomplete.getPlace()
-            console.log(addressObject)
-            if (!addressObject.geometry) return
-            const { formatted_address, geometry } = addressObject
-            callback(
-                formatted_address,
-                geometry.location.lat(),
-                geometry.location.lng()
-            )
-        })
-    }
-
-    return (
-        <Script
-            url="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5OfdpnZUgkR1LpThm7XMIeIJE49O2jZI&libraries=places"
-            onLoad={handleScriptLoad}
-        />
-    )
-}
-
-AutoComplete.propTypes = {
-    callback: PropTypes.func
-}
-
-AutoComplete.defaultProps = {
-    callback: () => null
+const AutoComplete = () => {
+    return <GooglePlacesAutocomplete onSelect={e => console.log(e)} />
 }
 
 export default AutoComplete
