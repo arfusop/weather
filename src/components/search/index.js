b32e4d55520a619658d5905a071abc937d247713
Phil Arfuso <Phil@Cathys-MacBook-Pro.local>
2020-05-17 11:35:58 -0400

    Started refactoring searhc bar

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaMapPin } from 'react-icons/fa'
 import { StyledSearch, StyledSuggestions } from './style'
 
 export const Search = ({ ...rest }) => {
     return (
         <StyledSearch transitionLength="0.6s">
-            <input id="react-google-places-autocomplete-input" {...rest} />
+            {/* <input id="react-google-places-autocomplete-input" {...rest} />
             <label for="react-google-places-autocomplete-input">
                 <FaSearch className="searchIcon" />
             </label>
-            <input type="submit" id="search-submit" />
+            <input type="submit" id="search-submit" /> */}
+
+            <input id="react-google-places-autocomplete-input" 
+            className="searchInput" {...rest} />
+            <div className="searchIcon">
+                <FaSearch className="search" />
+            </div>
         </StyledSearch>
     )
 }
@@ -21,9 +27,11 @@ export const Suggestions = ({ active, suggestions, onSelectSuggestion }) => {
                 const { description } = suggestion
                 return (
                     <div
+                        className="suggestion"
                         onClick={event =>
                             onSelectSuggestion(suggestion, event)
                         }>
+                        <FaMapPin />
                         {description}
                     </div>
                 )
