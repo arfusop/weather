5ca70958823d2a081eedf672b096b7c3ebd531ae
Phil Arfuso <Phil@Cathys-MacBook-Pro.local>
2020-05-18 21:28:19 -0400

    Added a search input

--- src/components/search/style.js
+++ src/components/search/style.js
@@ -1,40 +1,6 @@
 import styled from '@emotion/styled'
 
-export const StyledSearch = styled.section`
-    display: grid;
-    grid-template-columns: 1fr 2rem;
-    justify-content: center;
-    align-items: center;
-    /* width: 2rem; */
-    height: 2rem;
-    font-size: 1rem;
-    /* transition: width all 1s ease-out; */
-    transition: width 1s;
-
-    &:focus {
-        width: 100%;
-    }
-
-    .searchInput {
-        height: 100%;
-        border: none;
-        padding: 0 5px;
-    }
-
-    .searchIcon {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: white;
-    }
-
-    .search,
-    .searchInput {
-        font-size: 1.2rem;
-    }
-`
+export const StyledSearch = styled.section``
 
 export const StyledSuggestions = styled.section`
     .suggestion {
