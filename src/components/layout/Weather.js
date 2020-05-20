7de0262b13041aec28e04f7d74af1d344593ae9c
Phil Arfuso <Phil@Cathys-MacBook-Pro.local>
2020-05-19 22:12:58 -0400

    Added layout, tabs, weather, and location components

---
+++ src/components/layout/Weather.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import WeatherTabs from '../weatherTabs'
+
+const Weather = () => {
+    return <WeatherTabs />
+}
+
+export default Weather
---
+++ src/components/weatherTabs/style.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const StyledTabs = styled.section`
+    padding: 1rem;
+    background-color: rgba(21, 24, 17, 0.35);
+`
