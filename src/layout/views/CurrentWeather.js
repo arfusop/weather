6667ab2489ab6c87acb1b09ff69694846258942b
Phil <arfusop@gmail.com>
2020-05-22 22:30:52 -0400

    Moved more stuff to redux

--- src/layout/views/CurrentWeather.js
+++ src/layout/views/CurrentWeather.js
@@ -10,19 +10,12 @@ const StyledCurrentWeather = styled.section``
 const CurrentWeather = () => {
     const {
         location,
-        weather: { currently, daily, hourly, minutely }
+        weather: { currently, daily, hourly }
     } = useSelector(state => state.app)
     const currentDay = daily.data[0]
     const dayOfWeek = getDay(new Date())
     const date = format(new Date(), `${getDayOfWeek(dayOfWeek)}, MMM d`)
-    const town =
-        Object.keys(location).length > 0
-            ? location.address_components.filter(component => {
-                  if (component.types[0] === 'locality') {
-                      return component
-                  }
-              })
-            : ''
+
     return (
         <WeatherCard>
             <StyledCurrentWeather>
@@ -34,9 +27,7 @@ const CurrentWeather = () => {
                     </h4>
                 </div>
                 <div className="date">{date}</div>
-                <div className="location">
-                    {Object.keys(town).length > 0 ? town[0].short_name : ''}
-                </div>
+                <div className="location">{location.town}</div>
                 <div className="currentSummary">
                     <i className={`wi ${getWeatherIcon(hourly.icon)}`} />{' '}
                     {hourly.summary}
--- src/store/actions/user/index.js
+++ src/store/actions/user/index.js
@@ -1,6 +1,5 @@
 import { UAParser } from 'ua-parser-js'
 import { SET_USER_AGENT } from '../../types/user'
-import { SET_LOADING } from '../../types/app'
 
 export const getUserAgent = () => dispatch => {
     const parsed = new UAParser()
