b7ba97c8dc4b885903dff11eabfbebdd6af28e81
phil <arfusop@gmail.com>
2020-06-17 22:07:46 -0400

    started adding day data

--- src/layout/views/Daily.js
+++ src/layout/views/Daily.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { format } from 'date-fns'
 import WeatherCard from '../../components/card'
+import WeatherSpan from '../../components/weatherSpan'
+import { getWeatherIcon } from '../../helpers'
+import { StyledDaily } from './styled/StyledDaily'
 
 const Daily = () => {
-    return <WeatherCard>Daily card</WeatherCard>
+    const { daily } = useSelector(state => state.app.weather)
+
+    return (
+        <StyledDaily className="DailyCard">
+            <WeatherCard className="StyledDailyWeather">
+                <span className="title">Next 7 days</span>
+                <div className="dailyItemsContainer">
+                    {daily.data.map((day, index) => {
+                        const {
+                            temperatureMax,
+                            temperatureMin,
+                            precipProbability,
+                            precipType,
+                            time,
+                            icon
+                        } = day
+
+                        const displayPrecip = precipProbability > 0.09
+
+                        return (
+                            <div className="dailyItem" key={index}>
+                                <WeatherSpan
+                                    key={`dailyHighTemp ${index}`}
+                                    temp={temperatureMax}
+                                />
+                                <div className="indicator" />
+                                <WeatherSpan
+                                    key={`dailyLowTemp ${index}`}
+                                    temp={temperatureMin}
+                                />
+                                <div className="precipIndicator">
+                                    {displayPrecip ? (
+                                        <span>{precipProbability}%</span>
+                                    ) : null}
+                                </div>
+                                <i className={`wi ${getWeatherIcon(icon)}`} />
+                                <span className="dayOfWeek">
+                                    {format(new Date(time * 1000), 'EEE')}
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+            </WeatherCard>
+        </StyledDaily>
+    )
 }
 
 export default Daily
---
+++ src/layout/views/styled/StyledDaily.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const StyledDaily = styled.section`
+    width: 100%;
+    height: 90%;
+
+    .dailyItemsContainer {
+        display: flex;
+    }
+`
