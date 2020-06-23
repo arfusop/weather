4bb51a5d4a9e5d60036647d502414d848b72a806
phil <arfusop@gmail.com>
2020-06-23 00:06:49 -0400

    Removed unused variables

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { StyledLayout } from './style'
-import RSC from 'react-scrollbars-custom'
+// import RSC from 'react-scrollbars-custom'
 import CurrentWeather from './views/CurrentWeather'
 import CurrentDetails from './views/CurrentDetails'
 import Hourly from './views/Hourly'
